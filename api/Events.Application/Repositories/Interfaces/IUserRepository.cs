0cbcd204f47faafe57584eebc62259be1b1a452b
Ilya Katushenok <anotherfles@gmail.com>
2024-07-29 15:47:05 +0300

    User repository

---
+++ api/Events.Application/Repositories/Interfaces/IUserRepository.cs
@@ -0,0 +1,8 @@
+﻿using Events.Domain.Models;
+
+namespace Events.Application.Repositories.Interfaces;
+
+public interface IUserRepository : IRepositoryBase<User>
+{
+
+}
---
+++ api/Events.Infrastructure/Repositories/Implementations/UserRepository.cs
@@ -0,0 +1,16 @@
+﻿using Events.Application.Repositories.Interfaces;
+using Events.Domain.Models;
+using Events.Infrastructure.Context;
+
+namespace Events.Infrastructure.Repositories.Implementations;
+
+public class UserRepository : BaseRepository<User>, IUserRepository
+{
+    private readonly EventsDBContext _eventsDBContext;
+    public UserRepository(EventsDBContext eventsDBContext)
+        :base(eventsDBContext)
+    {
+        _eventsDBContext = eventsDBContext;
+    }
+
+}
