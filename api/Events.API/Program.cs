45794416fd0ef1b2e664125eb199a1191d2ff72b
Ilya Katushenok <anotherfles@gmail.com>
2024-07-29 23:08:46 +0300

    Install AutoMapper

--- api/Events.API/Program.cs
+++ api/Events.API/Program.cs
@@ -9,6 +9,7 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddAutoMapper(typeof(Program));
 builder.Services.AddDbContext<EventsDBContext>(options =>
 {
 	options.UseSqlServer(builder.Configuration.GetConnectionString("sqlConnection"));
