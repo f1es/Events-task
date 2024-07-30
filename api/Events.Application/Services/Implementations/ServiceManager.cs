61a3b4d53eb3afa6f2d1e802dee7a457f060b60a
Ilya Katushenok <anotherfles@gmail.com>
2024-07-30 19:12:33 +0300

    Add services manager class

---
+++ api/Events.Application/Services/Implementations/ServiceManager.cs
@@ -0,0 +1,29 @@
+﻿using AutoMapper;
+using Events.Application.Repositories.Interfaces;
+using Events.Application.Services.Interfaces;
+
+namespace Events.Application.Services.Implementations;
+
+public class ServiceManager : IServiceManager
+{
+    private readonly Lazy<IEventService> _eventService;
+    private readonly Lazy<IParticipantService> _participantService;
+    private readonly Lazy<IImageService> _imageService;
+    private readonly Lazy<IUserService> _userService;
+    public ServiceManager(IRepositoryManager repositoryManager, IMapper mapper)
+    {
+        _eventService = new Lazy<IEventService>(() =>
+        new EventService(repositoryManager, mapper));
+
+        _participantService = new Lazy<IParticipantService>(() =>
+        new ParticipantService(repositoryManager, mapper));
+    }
+
+    public IEventService EventService => _eventService.Value;
+
+	public IParticipantService ParticipantService => _participantService.Value;
+
+	public IImageService ImageService => throw new NotImplementedException();
+
+	public IUserService UserService => throw new NotImplementedException();
+}
---
+++ api/Events.Application/Services/Interfaces/IServiceManager.cs
@@ -0,0 +1,9 @@
+﻿namespace Events.Application.Services.Interfaces;
+
+public interface IServiceManager
+{
+	IEventService EventService { get; }
+	IParticipantService ParticipantService { get; }
+	IImageService ImageService { get; }
+	IUserService UserService { get; }
+}
