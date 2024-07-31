dc6799c2a7543a0f5df75db446998dc2293d5504
Ilya Katushenok <anotherfles@gmail.com>
2024-07-31 17:15:33 +0300

    Add user repository

--- api/Events.Application/Repositories/Interfaces/IUserRepository.cs
+++ api/Events.Application/Repositories/Interfaces/IUserRepository.cs
@@ -4,5 +4,10 @@ namespace Events.Application.Repositories.Interfaces;
 
 public interface IUserRepository 
 {
+	Task<User> GetByIdAsync(Guid id, bool trackChanges);
+	void CreateUser(User user);
+	void DeleteUser(User user);
+	void UpdateUser(User user);
 
 }
+
--- api/Events.Infrastructure/Repositories/Implementations/UserRepository.cs
+++ api/Events.Infrastructure/Repositories/Implementations/UserRepository.cs
@@ -1,6 +1,7 @@
 ﻿using Events.Application.Repositories.Interfaces;
 using Events.Domain.Models;
 using Events.Infrastructure.Context;
+using Microsoft.EntityFrameworkCore;
 
 namespace Events.Infrastructure.Repositories.Implementations;
 
@@ -13,4 +14,16 @@ public class UserRepository : BaseRepository<User>, IUserRepository
         _eventsDBContext = eventsDBContext;
     }
 
+	public void CreateUser(User user) =>
+		Create(user);
+
+	public void DeleteUser(User user) => 
+		Delete(user);
+
+	public async Task<User> GetByIdAsync(Guid id, bool trackChanges) =>
+		await GetByPredicate(u => u.Id.Equals(id), trackChanges)
+		.SingleOrDefaultAsync();
+
+	public void UpdateUser(User user) => 
+		Update(user);
 }
