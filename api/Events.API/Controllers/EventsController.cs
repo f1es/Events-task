761783942179dc50cbce284c8c5f9a54dae1e45f
Ilya Katushenok <anotherfles@gmail.com>
2024-07-31 17:17:52 +0300

    Add status codes to events controller

--- api/Events.API/Controllers/EventsController.cs
+++ api/Events.API/Controllers/EventsController.cs
@@ -2,56 +2,61 @@
 using Events.Domain.Shared.DTO.Request;
 using Microsoft.AspNetCore.Mvc;
 
-namespace Events.API.Controllers
+namespace Events.API.Controllers;
+
+[ApiController]
+[Route("api/events")]
+public class EventsController : ControllerBase
 {
-    [ApiController]
-    [Route("api/events")]
-    public class EventsController : ControllerBase
+    private readonly IServiceManager _serviceManager;
+    public EventsController(IServiceManager serviceManager)
     {
-        private readonly IServiceManager _serviceManager;
-        public EventsController(IServiceManager serviceManager)
-        {
-            _serviceManager = serviceManager;
-        }
+        _serviceManager = serviceManager;
+    }
 
-        [HttpGet]
-        public async Task<IActionResult> GetEvents()
-        {
-            var events = await _serviceManager.EventService.GetAllEventsAsync(trackChanges: false);
+    [HttpGet]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    public async Task<IActionResult> GetEvents()
+    {
+        var events = await _serviceManager.EventService.GetAllEventsAsync(trackChanges: false);
 
-            return Ok(events);
-        }
+        return Ok(events);
+    }
 
-        [HttpGet("{id:guid}", Name = "EventById")]
-        public async Task<IActionResult> GetEvent(Guid id)
-        {
-            var eventResponse = await _serviceManager.EventService.GetEventByIdAsync(id, trackChanges: false);
+    [HttpGet("{id:guid}", Name = "EventById")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    public async Task<IActionResult> GetEvent(Guid id)
+    {
+        var eventResponse = await _serviceManager.EventService.GetEventByIdAsync(id, trackChanges: false);
 
-            return Ok(eventResponse);
-        }
+        return Ok(eventResponse);
+    }
 
-        [HttpPost]
-        public async Task<IActionResult> CreateEvent([FromBody] EventForCreateRequestDto eventModel)
-        {
-            var eventResponse = await _serviceManager.EventService.CreateEventAsync(eventModel);
+    [HttpPost]
+    [ProducesResponseType(StatusCodes.Status201Created)]
+    public async Task<IActionResult> CreateEvent([FromBody] EventForCreateRequestDto eventModel)
+    {
+        var eventResponse = await _serviceManager.EventService.CreateEventAsync(eventModel);
 
-            return Ok(eventResponse);
-        }
+        return CreatedAtRoute("EventById", new { id = eventResponse.Id }, eventResponse);
+    }
 
-        [HttpPut("{id:guid}")]
-        public async Task<IActionResult> UpdateEvent(Guid id, [FromBody] EventForUpdateRequestDto eventModel)
-        {
-            await _serviceManager.EventService.UpdateEventAsync(id, eventModel, trackChanges: true);
+    [HttpPut("{id:guid}")]
+    [ProducesResponseType(StatusCodes.Status204NoContent)]
+    public async Task<IActionResult> UpdateEvent(Guid id, [FromBody] EventForUpdateRequestDto eventModel)
+    {
+        await _serviceManager.EventService.UpdateEventAsync(id, eventModel, trackChanges: true);
 
-            return NoContent();
-        }
+        return NoContent();
+    }
 
-        [HttpDelete("{id:guid}")]
-        public async Task<IActionResult> DeleteEvent(Guid id)
-        {
-            await _serviceManager.EventService.DeleteEventAsync(id, trackChanges: false);
+    [HttpDelete("{id:guid}")]
+	[ProducesResponseType(StatusCodes.Status204NoContent)]
+	public async Task<IActionResult> DeleteEvent(Guid id)
+    {
+        await _serviceManager.EventService.DeleteEventAsync(id, trackChanges: false);
 
-            return NoContent();
-        }
+        return NoContent();
     }
 }
