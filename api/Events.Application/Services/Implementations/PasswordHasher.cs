0f7d575e048cbaa9b45646c7c8121def65c85a71
Ilya Katushenok <anotherfles@gmail.com>
2024-08-02 18:33:30 +0300

    Add password hasher implementation

---
+++ api/Events.Application/Services/Implementations/PasswordHasher.cs
@@ -0,0 +1,12 @@
+﻿using Events.Application.Services.Interfaces;
+
+namespace Events.Application.Services.Implementations;
+
+public class PasswordHasher : IPasswordHasher
+{
+	public string GenerateHash(string password) => 
+		BCrypt.Net.BCrypt.EnhancedHashPassword(password);
+
+	public bool VerifyPassword(string password, string passwordHash) => 
+		BCrypt.Net.BCrypt.EnhancedVerify(password, passwordHash);
+}
