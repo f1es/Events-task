72e1d5382a1aa2927e66f66dac6d065f6b496ba2
Ilya Katushenok <anotherfles@gmail.com>
2024-08-06 16:48:29 +0300

    Add extension to get message from validation result

---
+++ api/Events.Application/Extensions/ValidationResultExtensions.cs
@@ -0,0 +1,21 @@
+﻿using FluentValidation.Results;
+
+namespace Events.Application.Extensions;
+
+public static class ValidationResultExtensions
+{
+	public static string GetMessage(this ValidationResult validationResult)
+	{
+		var message = string.Empty;
+
+		foreach(var error in validationResult.Errors)
+		{
+			message += error.ErrorMessage;
+			
+			if (error != validationResult.Errors.Last())
+				message += ", ";
+		}
+
+		return message;
+	}
+}
