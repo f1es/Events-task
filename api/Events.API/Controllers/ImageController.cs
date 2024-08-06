2d5775b68b2d7a9b94bd62a04fe24e8b8071a2ab
Ilya Katushenok <anotherfles@gmail.com>
2024-08-06 15:28:44 +0300

    Add image controller

---
+++ api/Events.API/Controllers/ImageController.cs
@@ -0,0 +1,37 @@
+﻿using Events.Application.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Events.API.Controllers;
+
+[ApiController]
+[Route("api/events/{eventId:guid}/image")]
+public class ImageController : ControllerBase
+{
+	private readonly IServiceManager _serviceManager;
+    public ImageController(IServiceManager serviceManager)
+    {
+        _serviceManager = serviceManager;
+    }
+    [HttpPost]
+	public async Task<IActionResult> UploadImage(Guid eventId,  IFormFile image)
+	{
+		await _serviceManager.ImageService.UploadImageAsync(eventId, image, trackChanges: false);
+
+		return NoContent();
+	}
+
+	[HttpGet]
+	public async Task<IActionResult> GetImage(Guid eventId)
+	{
+		var image = await _serviceManager.ImageService.GetImageAsync(eventId, trackChanges: false);
+
+		return File(image.Content, image.Type);
+	}
+	[HttpPut]
+	public async Task<IActionResult> UpdateImage(Guid eventId, IFormFile image)
+	{
+		await _serviceManager.ImageService.UpdateImageAsync(eventId, image, trackChanges: true);
+
+		return NoContent();
+	}
+}
