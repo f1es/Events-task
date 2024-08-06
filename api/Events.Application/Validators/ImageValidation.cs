2c192fcd4660558df078f77776eb05b8a2e4fd2c
Ilya Katushenok <anotherfles@gmail.com>
2024-08-06 16:48:55 +0300

    Add image validator

---
+++ api/Events.Application/Validators/ImageValidation.cs
@@ -0,0 +1,20 @@
+﻿using FluentValidation;
+using Microsoft.AspNetCore.Http;
+
+namespace Events.Application.Validators;
+
+public class ImageValidation : AbstractValidator<IFormFile>
+{
+    public ImageValidation()
+    {
+        RuleFor(i => i.Length)
+            .LessThan(2000000).WithMessage("The image cannot be larger than 2 mb");
+
+        RuleFor(i => i.ContentType)
+            .Must(type => 
+            type.Equals("image/png") ||
+            type.Equals("image/jpeg") ||
+            type.Equals("image/jpg")).WithMessage("The uploaded file must be an image");
+            
+    }
+}
