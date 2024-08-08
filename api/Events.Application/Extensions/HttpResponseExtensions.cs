4536bfabd8b889b813663fb52f0098cf779dbe32
Ilya Katushenok <anotherfles@gmail.com>
2024-08-08 19:53:58 +0300

    Add extensions to append tokens to cookie

---
+++ api/Events.Application/Extensions/HttpResponseExtensions.cs
@@ -0,0 +1,21 @@
+﻿using Events.Domain.Models;
+using Microsoft.AspNetCore.Http;
+
+namespace Events.Application.Extensions;
+
+public static class HttpResponseExtensions
+{
+	public static void AppendRefreshToken(this HttpResponse response, RefreshToken refreshToken)
+	{
+		var cookieOptions = new CookieOptions
+		{
+			HttpOnly = true,
+			Expires = refreshToken.Expires,
+		};
+
+		response.Cookies.Append("ref", refreshToken.Token);
+	}
+
+	public static void AppendAccessToken(this HttpResponse response, string accessToken) =>
+		response.Cookies.Append("cook", accessToken);
+}
