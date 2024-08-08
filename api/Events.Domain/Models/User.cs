68bc5df7f13f9b9fe9ed570a1ca6ec69355e673d
Ilya Katushenok <anotherfles@gmail.com>
2024-08-08 19:53:14 +0300

    Refresh token in user

--- api/Events.Domain/Models/User.cs
+++ api/Events.Domain/Models/User.cs
@@ -7,5 +7,7 @@ public class User
 	public string? Email { get; set; }
 	public string? PasswordHash { get; set; }
 
+
+	public RefreshToken RefreshToken { get; set; }
 	public ICollection<Participant>? Participants { get; set; }
 }
