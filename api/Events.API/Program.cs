5ca6b590452679204cde80eeb13b615b8a2cec5a
Ilya Katushenok <anotherfles@gmail.com>
2024-08-14 23:06:36 +0300

    Configure tokens options

--- api/Events.API/Program.cs
+++ api/Events.API/Program.cs
@@ -11,6 +11,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.Configure<JwtOptions>(builder.Configuration.GetSection(nameof(JwtOptions)));
+builder.Services.Configure<RefreshTokenOptions>(builder.Configuration.GetSection(nameof(RefreshTokenOptions)));
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
---
+++ api/Events.Application/Options/RefreshTokenOptions.cs
@@ -0,0 +1,6 @@
+﻿namespace Events.Application.Options;
+
+public class RefreshTokenOptions
+{
+	public int ExpiresDays { get; set; }
+}
