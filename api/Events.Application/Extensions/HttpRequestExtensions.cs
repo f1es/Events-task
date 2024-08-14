f99881999ee97609ffc9ca3b3d71f9f06fa2ced9
Ilya Katushenok <anotherfles@gmail.com>
2024-08-14 23:04:29 +0300

    Add extension to store refresh token in cookie

---
+++ api/Events.Application/Extensions/HttpRequestExtensions.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Http;
+using System.Runtime.CompilerServices;
+
+namespace Events.Application.Extensions;
+
+public static class HttpRequestExtensions
+{
+	public static string GetRefreshToken(this HttpRequest httpRequest)
+	{
+		httpRequest.Cookies.TryGetValue("ref", out string refreshToken);
+
+		return refreshToken;
+	}
+}
