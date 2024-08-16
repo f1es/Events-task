8101a5f48db710ddaa23415d844722fe7c5ff583
Ilya Katushenok <anotherfles@gmail.com>
2024-08-16 17:17:26 +0300

    Add generic pagination method

---
+++ api/Events.Infrastructure/Extensions/GenericRepositoriesExtensions.cs
@@ -0,0 +1,20 @@
+﻿using Events.Domain.Exceptions;
+
+namespace Events.Infrastructure.Extensions;
+
+public static class GenericRepositoriesExtensions
+{
+	public static IQueryable<T> Paginate<T>(this IQueryable<T> query, int page, int pageSize)
+	{
+		if (page <= 0 || pageSize <= 0)
+		{
+			throw new BadRequestException("Page or page size must be larger than zero");
+		}
+
+		query = query
+			.Skip((page - 1) * pageSize)
+			.Take(pageSize);
+
+		return query;
+	}
+}
