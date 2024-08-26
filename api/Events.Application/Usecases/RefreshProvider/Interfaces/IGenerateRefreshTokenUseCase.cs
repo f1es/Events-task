84e5a04c8e170bb3dec687719d62e43ba3de4ea4
Ilya Katushenok <anotherfles@gmail.com>
2024-08-26 20:08:45 +0300

    Create refreshProvider usecase interface

---
+++ api/Events.Application/Usecases/RefreshProvider/Interfaces/IGenerateRefreshTokenUseCase.cs
@@ -0,0 +1,8 @@
+﻿using Events.Domain.Models;
+
+namespace Events.Application.Usecases.RefreshProvider.Interfaces;
+
+public interface IGenerateRefreshTokenUseCase
+{
+	RefreshToken GenerateToken(Guid userId);
+}
