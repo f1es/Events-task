1de1ae12c20871af1cfc8516e536f70fdf719424
Ilya Katushenok <anotherfles@gmail.com>
2024-08-26 20:09:13 +0300

    Create user usecase interfaces

---
+++ api/Events.Application/Usecases/User/Interfaces/IGetUserByIdUseCase.cs
@@ -0,0 +1,8 @@
+﻿using Events.Domain.Shared.DTO.Response;
+
+namespace Events.Application.Usecases.User.Interfaces;
+
+public interface IGetUserByIdUseCase
+{
+	Task<UserResponseDto> GetUserByIdAsync(Guid id, bool trackChanges);
+}
---
+++ api/Events.Application/Usecases/User/Interfaces/IRegisterUserUseCase.cs
@@ -0,0 +1,8 @@
+﻿using Events.Domain.Shared.DTO.Request;
+
+namespace Events.Application.Usecases.User.Interfaces;
+
+public interface IRegisterUserUseCase
+{
+	Task RegisterUserAsync(UserRegisterRequestDto user, bool trackChanges);
+}
