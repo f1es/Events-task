c1e82de7fa00088e1e8c84935433c046fcd85033
Ilya Katushenok <anotherfles@gmail.com>
2024-08-26 20:08:29 +0300

    Create passwordHasher usecase interfaces

---
+++ api/Events.Application/Usecases/PasswordHasher/Interfaces/IGenerateHashUseCase.cs
@@ -0,0 +1,6 @@
+﻿namespace Events.Application.Usecases.PasswordHasher.Interfaces;
+
+public interface IGenerateHashUseCase
+{
+	string GenerateHash(string password);
+}
---
+++ api/Events.Application/Usecases/PasswordHasher/Interfaces/IVerifyPasswordUseCase.cs
@@ -0,0 +1,6 @@
+﻿namespace Events.Application.Usecases.PasswordHasher.Interfaces;
+
+public interface IVerifyPasswordUseCase
+{
+	bool VerifyPassword(string password, string passwordHash);
+}
