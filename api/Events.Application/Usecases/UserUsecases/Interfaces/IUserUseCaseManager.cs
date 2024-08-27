6d16a996fb89cd6c0171923f9b76433653e4b893
Ilya Katushenok <anotherfles@gmail.com>
2024-08-27 20:49:31 +0300

    Add UserUseCaseManager

---
+++ api/Events.Application/Usecases/UserUsecases/Interfaces/IUserUseCaseManager.cs
@@ -0,0 +1,11 @@
+﻿namespace Events.Application.Usecases.UserUsecases.Interfaces
+{
+    public interface IUserUseCaseManager
+    {
+        IGetAllUsersUseCase GetAllUsersUseCase { get; }
+        IGetUserByIdUseCase GetUserByIdUseCase { get; }
+        IGrantRoleForUserUseCase GrantRoleForUserUseCase { get; }
+        ILoginUserUseCase LoginUserUseCase { get; }
+        IRegisterUserUseCase RegisterUserUseCase { get; }
+    }
+}
