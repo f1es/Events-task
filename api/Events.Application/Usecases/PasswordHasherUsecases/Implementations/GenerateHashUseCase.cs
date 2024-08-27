9fb98e90d2464ce8f1941517c7b9408d9d140d48
Ilya Katushenok <anotherfles@gmail.com>
2024-08-27 16:52:56 +0300

    Add PasswordHasher usecases

---
+++ api/Events.Application/Usecases/PasswordHasherUsecases/Implementations/GenerateHashUseCase.cs
@@ -0,0 +1,9 @@
+﻿using Events.Application.Usecases.PasswordHasherUsecases.Interfaces;
+
+namespace Events.Application.Usecases.PasswordHasherUsecases.Implementations;
+
+public class GenerateHashUseCase : IGenerateHashUseCase
+{
+	public string GenerateHash(string password) =>
+		BCrypt.Net.BCrypt.EnhancedHashPassword(password);
+}
---
+++ api/Events.Application/Usecases/PasswordHasherUsecases/Implementations/VerifyPasswordUseCase.cs
@@ -0,0 +1,9 @@
+﻿using Events.Application.Usecases.PasswordHasherUsecases.Interfaces;
+
+namespace Events.Application.Usecases.PasswordHasherUsecases.Implementations;
+
+public class VerifyPasswordUseCase : IVerifyPasswordUseCase
+{
+	public bool VerifyPassword(string password, string passwordHash) =>
+		BCrypt.Net.BCrypt.EnhancedVerify(password, passwordHash);
+}
