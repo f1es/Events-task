b0040118863394b96457913ee860cfd010db7f59
Ilya Katushenok <anotherfles@gmail.com>
2024-08-27 20:48:25 +0300

    Add ParticipantUseCaseManager

---
+++ api/Events.Application/Usecases/ParticipantUsecases/Interfaces/IParticipantUseCaseManager.cs
@@ -0,0 +1,11 @@
+﻿namespace Events.Application.Usecases.ParticipantUsecases.Interfaces
+{
+    public interface IParticipantUseCaseManager
+    {
+        ICreateParticipantUseCase CreateParticipantUseCase { get; }
+        IDeleteParticipantUseCase DeleteParticipantUseCase { get; }
+        IGetAllParticipantsUseCase GetAllParticipantsUseCase { get; }
+        IGetParticipantByIdUseCase GetParticipantByIdUseCase { get; }
+        IUpdateParticipantUseCase UpdateParticipantUseCase { get; }
+    }
+}
