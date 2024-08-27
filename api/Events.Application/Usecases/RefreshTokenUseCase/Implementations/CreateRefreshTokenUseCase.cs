340fe727d2ea85eabdae1ce309dcc96aa933e168
Ilya Katushenok <anotherfles@gmail.com>
2024-08-27 17:18:51 +0300

    Add RefreshToken usecases

---
+++ api/Events.Application/Usecases/RefreshTokenUseCase/Implementations/CreateRefreshTokenUseCase.cs
@@ -0,0 +1,30 @@
+﻿using AutoMapper;
+using Events.Application.Services.SecurityServices.Interfaces;
+using Events.Application.Usecases.RefreshProviderUsecase.Interfaces;
+using Events.Application.Usecases.RefreshTokenUseCase.Interfaces;
+using Events.Domain.Repositories.Interfaces;
+
+namespace Events.Application.Usecases.RefreshTokenUseCase.Implementations;
+
+public class CreateRefreshTokenUseCase : ICreateRefreshTokenUseCase
+{
+	private readonly IRepositoryManager _repositoryManager;
+	private readonly IGenerateRefreshTokenUseCase _generateRefreshTokenUseCase;
+
+	public CreateRefreshTokenUseCase(IRepositoryManager repositoryManager, IGenerateRefreshTokenUseCase generateRefreshTokenUseCase)
+	{
+		_repositoryManager = repositoryManager;
+		_generateRefreshTokenUseCase = generateRefreshTokenUseCase;
+	}
+
+	public async Task CreateRefreshTokenAsync(Guid userId)
+	{
+		{
+			var token = _generateRefreshTokenUseCase.GenerateToken(userId);
+
+			_repositoryManager.RefreshToken.Create(token);
+
+			await _repositoryManager.SaveAsync();
+		}
+	}
+}
---
+++ api/Events.Application/Usecases/RefreshTokenUseCase/Implementations/UpdateRefreshTokenUseCase.cs
@@ -0,0 +1,27 @@
+﻿using AutoMapper;
+using Events.Application.Usecases.RefreshTokenUseCase.Interfaces;
+using Events.Domain.Models;
+using Events.Domain.Repositories.Interfaces;
+
+namespace Events.Application.Usecases.RefreshTokenUseCase.Implementations;
+
+public class UpdateRefreshTokenUseCase : IUpdateRefreshTokenUseCase
+{
+	private readonly IRepositoryManager _repositoryManager;
+	private readonly IMapper _mapper;
+
+	public UpdateRefreshTokenUseCase(IRepositoryManager repositoryManager, IMapper mapper)
+	{
+		_repositoryManager = repositoryManager;
+		_mapper = mapper;
+	}
+
+	public async Task UpdateRefreshToken(Guid userId, RefreshToken refreshToken, bool trackChanges)
+	{
+		var userToken = await _repositoryManager.RefreshToken.GetRefreshTokenByUserIdAsync(userId, trackChanges);
+
+		userToken = _mapper.Map(refreshToken, userToken);
+
+		await _repositoryManager.SaveAsync();
+	}
+}
