b4a31fd2af9e696b03f63e9e7f2bbf2da587b400
Ilya Katushenok <anotherfles@gmail.com>
2024-08-27 16:08:42 +0300

    Add jwtProvider usecases

---
+++ api/Events.Application/Usecases/JwtProviderUsecases/Implementations/GenerateTokenUseCase.cs
@@ -0,0 +1,39 @@
+﻿using Events.Application.Options;
+using Events.Application.Usecases.JwtProviderUsecases.Interfaces;
+using Events.Domain.Models;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace Events.Application.Usecases.JwtProviderUsecases.Implementations;
+
+public class GenerateTokenUseCase : IGenerateTokenUseCase
+{
+	private readonly JwtOptions _jwtOptions;
+	public GenerateTokenUseCase(IOptions<JwtOptions> jwtOptions)
+	{
+		_jwtOptions = jwtOptions.Value;
+	}
+	public string GenerateToken(User user)
+	{
+		Claim[] claims = [
+			new("userId", user.Id.ToString()),
+			new("role", user.Role)
+			];
+
+		var signingCredentials = new SigningCredentials(
+			new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_jwtOptions.Key)),
+		SecurityAlgorithms.HmacSha256);
+
+		var token = new JwtSecurityToken(
+			claims: claims,
+			signingCredentials: signingCredentials,
+			expires: DateTime.UtcNow.AddHours(_jwtOptions.ExpiresHours));
+
+		var tokenValue = new JwtSecurityTokenHandler().WriteToken(token);
+
+		return tokenValue;
+	}
+}
