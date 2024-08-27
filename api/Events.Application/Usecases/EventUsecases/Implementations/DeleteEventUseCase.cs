c5c95a8c7e2cfdfb8b02e3f07d292ef0c9c15968
Ilya Katushenok <anotherfles@gmail.com>
2024-08-27 20:47:43 +0300

    Add EventUseCaseManager

--- api/Events.Application/Usecases/EventUsecases/Implementations/DeleteEventUseCase.cs
+++ api/Events.Application/Usecases/EventUsecases/Implementations/DeleteEventUseCase.cs
@@ -8,13 +8,9 @@ namespace Events.Application.Usecases.EventUsecases.Implementations;
 public class DeleteEventUseCase : IDeleteEventUseCase
 {
 	private readonly IRepositoryManager _repositoryManager;
-	private readonly IMapper _mapper;
-	public DeleteEventUseCase(
-		IRepositoryManager repositoryManager,
-		IMapper mapper)
+	public DeleteEventUseCase(IRepositoryManager repositoryManager)
     {
         _repositoryManager = repositoryManager;
-		_mapper = mapper;
     }
     public async Task DeleteEventAsync(Guid id, bool trackChanges)
     {
---
+++ api/Events.Application/Usecases/EventUsecases/Implementations/EventUseCaseManager.cs
@@ -0,0 +1,50 @@
+﻿using AutoMapper;
+using Events.Application.Usecases.EventUsecases.Interfaces;
+using Events.Domain.Repositories.Interfaces;
+
+namespace Events.Application.Usecases.EventUsecases.Implementations;
+
+public class EventUseCaseManager : IEventUseCaseManager
+{
+	private readonly Lazy<ICreateEventUseCase> _createEventUseCase;
+	private readonly Lazy<IDeleteEventUseCase> _deleteEventUseCase;
+	private readonly Lazy<IGetAllEventsUseCase> _getAllEventsUseCase;
+	private readonly Lazy<IGetEventByIdUseCase> _getEventByIdUseCase;
+	private readonly Lazy<IUpdateEventUseCase> _updateEventUseCase;
+
+	public EventUseCaseManager(
+		IRepositoryManager repositoryManager,
+		IMapper mapper)
+	{
+		_createEventUseCase = new Lazy<ICreateEventUseCase>(() =>
+		new CreateEventUseCase(
+			repositoryManager,
+			mapper));
+
+		_deleteEventUseCase = new Lazy<IDeleteEventUseCase>(() =>
+		new DeleteEventUseCase(repositoryManager));
+
+		_getAllEventsUseCase = new Lazy<IGetAllEventsUseCase>(() =>
+		new GetAllEventsUseCase(repositoryManager, mapper));
+
+		_getEventByIdUseCase = new Lazy<IGetEventByIdUseCase>(() =>
+		new GetEventByIdUseCase(
+			repositoryManager,
+			mapper));
+
+		_updateEventUseCase = new Lazy<IUpdateEventUseCase>(() =>
+		new UpdateEventUseCase(
+			repositoryManager,
+			mapper));
+	}
+
+	public ICreateEventUseCase CreateEventUseCase => _createEventUseCase.Value;
+
+	public IDeleteEventUseCase DeleteEventUseCase => _deleteEventUseCase.Value;
+
+	public IGetAllEventsUseCase GetAllEventsUseCase => _getAllEventsUseCase.Value;
+
+	public IGetEventByIdUseCase GetEventByIdUseCase => _getEventByIdUseCase.Value;
+
+	public IUpdateEventUseCase UpdateEventUseCase => _updateEventUseCase.Value;
+}
