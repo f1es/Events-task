8da25a934cff4aca598f250d4175a21290020a5c
Ilya Katushenok <anotherfles@gmail.com>
2024-08-29 02:25:55 +0300

    Add usecases di

--- api/Events.Application/Extensions/ApplicationExtensions.cs
+++ api/Events.Application/Extensions/ApplicationExtensions.cs
@@ -5,6 +5,22 @@ using Events.Application.Services.ModelServices.Implementations;
 using Events.Application.Services.ModelServices.Interfaces;
 using Events.Application.MapperProfiles;
 using FluentValidation.AspNetCore;
+using Events.Application.Usecases.EventUsecases.Interfaces;
+using Events.Application.Usecases.EventUsecases.Implementations;
+using Events.Application.Usecases.ImageUsecases.Interfaces;
+using Events.Application.Usecases.ImageUsecases.Implementations;
+using Events.Application.Usecases.JwtProviderUsecases.Interfaces;
+using Events.Application.Usecases.JwtProviderUsecases.Implementations;
+using Events.Application.Usecases.ParticipantUsecases.Interfaces;
+using Events.Application.Usecases.ParticipantUsecases.Implementations;
+using Events.Application.Usecases.PasswordHasherUsecases.Interfaces;
+using Events.Application.Usecases.PasswordHasherUsecases.Implementations;
+using Events.Application.Usecases.RefreshProviderUsecase.Interfaces;
+using Events.Application.Usecases.RefreshProviderUsecase.Implementations;
+using Events.Application.Usecases.RefreshTokenUseCase.Implementations;
+using Events.Application.Usecases.RefreshTokenUseCase.Interfaces;
+using Events.Application.Usecases.UserUsecases.Interfaces;
+using Events.Application.Usecases.UserUsecases.Implementations;
 
 namespace Events.Application.Extensions;
 
@@ -18,6 +34,18 @@ public static class ApplicationExtensions
 
 	public static void ConfigureServices(this IServiceCollection services) => 
 		services.AddScoped<IServiceManager, ServiceManager>();
+
+	public static void ConfigureUsecases(this IServiceCollection services)
+	{
+		services.AddScoped<IEventUseCaseManager, EventUseCaseManager>();
+		services.AddScoped<IImageUseCaseManager, ImageUseCaseManager>();
+		services.AddScoped<IJwtProviderUseCaseManager, JwtProviderUseCaseManager>();
+		services.AddScoped<IParticipantUseCaseManager, ParticipantUseCaseManager>();
+		services.AddScoped<IPasswordHasherUseCaseManager, PasswordHasherUseCaseManager>();
+		services.AddScoped<IRefreshProviderUseCaseManager, RefreshProviderUseCaseManager>();
+		services.AddScoped<IRefreshTokenUseCaseManager, RefreshTokenUseCaseManager>();
+		services.AddScoped<IUserUseCaseManager, UserUseCaseManager>();
+	}
 
 	public static void ConfigureMapperProfiles(this IServiceCollection services)
 	{
